aaeadde873c1f7fc0c793e1ebabbb198780b6164
Ricardo Juárez Tepos <45005831+RichieSjt@users.noreply.github.com>
2021-10-20 12:20:07 -0500

    NodeJS server

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const path = require('path')
+const express = require('express')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+// Define paths for express config
+const publicDirectoryPath = path.join(__dirname, '..', 'public')
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
+// Application routes
+app.get('', (req, res) => {
+    res.render('index')
+})
+
+app.listen(port, () =>{
+    console.log('Server is up on port ' + port + '.')
+})
